917406f75cdc009b71bdcfde1aeb9196621931df
TheCanMoun10 <58854102+TheCanMoun10@users.noreply.github.com>
2020-11-26 15:19:59 +0100

    Add files via upload

--- TCPServer.py
+++ TCPServer.py
@@ -2,6 +2,32 @@ import socket
 import time
 import numpy as np
 import xml.etree.ElementTree as ET
+import pandas as pd
+from numpy import genfromtxt
+import csv
+from csv import reader
+
+
+#proctimes = genfromtxt('procssing_times_table.csv', delimiter=';')
+#print(proctimes)
+
+with open('procssing_times_table.csv', 'r') as readfile_obj:
+
+    csv_reader = reader(readfile_obj, delimiter=';')
+    list_of_rows = list(csv_reader)
+    #print(list_of_rows)
+    for row in csv_reader:
+        print(row[])
+
+   #row_number = 3
+   #column_number = 2
+
+   #entrance = list_of_rows[row_number][column_number]
+    #   print(entrance)
+
+
+
+
 
 host = '192.168.0.102' #Check ipconfig to get IP, if not static.
 port = 22000
@@ -30,5 +56,9 @@ for reading in list(root):
     carrierID = int(reading.find('carrierID').text)
     carrierNum = 'Carrier#' + str(carrierID)
     stationID = int(reading.find('stationID').text)
+    stationNum = 'Station#0' + str(stationID)
     print('carrierID: %s;' %carrierID, 'stationID: %s' %stationID)
 
+#row = df.loc[carrierNum]
+#print(row)
+#column = df.loc[stationID]
